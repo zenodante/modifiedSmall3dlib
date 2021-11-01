bda7b41ce7e3a500709a4dc080b3dba2546254ec
zenodante <yeming@outlook.com>
2021-11-01 14:48:32 +0800

    Update S3L_config.h

--- S3L_config.h
+++ S3L_config.h
@@ -8,6 +8,12 @@ extern "C" {
 #define S3L_RESOLUTION_X       120
 #define S3L_RESOLUTION_Y       120
 
+
+/** How many fractions a spatial unit is split into. This is NOT SUPPOSED TO
+BE REDEFINED, so rather don't do it (otherwise things may overflow etc.). */
+
+#define S3L_FRACTIONS_PER_UNIT 512
+
 #define S3L_NEAR_CROSS_STRATEGY 2
 
 #define S3L_FLAT 0  
@@ -16,11 +22,11 @@ extern "C" {
 
 #define S3L_COMPUTE_DEPTH 0
 
-#define S3L_Z_BUFFER 0 
+#define S3L_Z_BUFFER 1
 #define S3L_REDUCED_Z_BUFFER_GRANULARITY 5
 
-#define S3L_STENCIL_BUFFER 1
-#define S3L_SORT 2
+#define S3L_STENCIL_BUFFER 0
+#define S3L_SORT 0
 
 #define S3L_MAX_TRIANGES_DRAWN 128
 
@@ -28,7 +34,6 @@ extern "C" {
 #define S3L_FAST_LERP_QUALITY 11 
 
 
-#define S3L_PIXEL_FUNCTION
   
 #ifdef __cplusplus
 }
